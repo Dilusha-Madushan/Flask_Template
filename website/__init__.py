f6779917b45afc169ec7760d10eadcec733f4dc7
Dilusha <dilushamadushan@gmail.com>
2022-05-18 22:16:13 +0530

    Finish login , signup and logout

--- website/__init__.py
+++ website/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+
     from .views import views
     from .auth import auth
 
@@ -21,6 +22,14 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
